8b110b27504ab6e1b62802db048c2d19bc04390d
unknown <gustavo.bordignon@outlook.com>
2024-04-04 22:17:40 -0300

    style the pagination

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Styles from './style.css';
 
+import Sol from '../../assets/images/planets/Sol.png';
+import Lua from '../../assets/images/planets/lua.png';
+import Mercurio from '../../assets/images/planets/mercurio.png';
+import Venus from '../../assets/images/planets/venus.png';
+import Terra from '../../assets/images/planets/terra.png';
+import Marte from '../../assets/images/planets/marte.png';
+import Jupiter from '../../assets/images/planets/jupiter.png';
+import Saturno from '../../assets/images/planets/saturno.png';
+import Urano from '../../assets/images/planets/urano.png';
+import Netuno from '../../assets/images/planets/netuno.png';
+
 const Pagination = ({ currentPage, onPageClick }) => {
   const totalPages = 10;
+  const images = [Sol, Lua, Mercurio, Venus, Terra, Marte, Jupiter, Saturno, Urano, Netuno];
 
   return (
     <ul className='Pagination'>
       {[...Array(totalPages).keys()].map(pageNumber => (
         <li key={pageNumber + 1} className={currentPage === pageNumber + 1 ? 'active' : ''}>
-          <button onClick={() => onPageClick(pageNumber + 1)}>
+          <button
+            onClick={() => onPageClick(pageNumber + 1)}
+            style={{ backgroundImage: `url(${images[pageNumber]})` }}
+          >
             {pageNumber + 1}
           </button>
         </li>
@@ -18,4 +33,3 @@ const Pagination = ({ currentPage, onPageClick }) => {
 };
 
 export default Pagination;
-
